c1f408d9b1d634e610848a126b4b4ddf5576a4c7
zeratul117 <luisbelis@hotmail.com>
2025-03-14 22:06:17 -0400

    adding some responsive features

--- src/app/(frontend)/loading.tsx
+++ src/app/(frontend)/loading.tsx
@@ -1,10 +1,27 @@
+"use client"
 import { Spin } from 'antd';
+import { useEffect, useState } from 'react';
 
 export default function Loading() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsVisible((prev) => !prev); 
+    }, 1000); 
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div className="p-10">
+  <div className="absolute inset-0 flex items-center justify-center bg-opacity-50">
+    <div
+      className={`transition-opacity duration-1000 ${
+        isVisible ? 'opacity-100' : 'opacity-0'
+      }`}
+    >
       <Spin size="large" />
     </div>
-  )
+  </div>
+  );
 }
